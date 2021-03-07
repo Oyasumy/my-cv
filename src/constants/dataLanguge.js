e9db619f344ca3093985376217c5fd35927f8959
Oyasumy <xuancuong99.it@gmail.com>
2021-03-07 23:42:32 +0700

    fix data language

--- src/constants/dataLanguge.js
+++ src/constants/dataLanguge.js
@@ -18,7 +18,7 @@ export const dataE = {
   },
   education: [
     {
-      h3: "Started studying at HUFLIT University",
+      h3: "Start studying at HUFLIT University",
       h4: "Majoring in Information Technology",
       p: "Student",
       time: "September 2017",
@@ -26,7 +26,7 @@ export const dataE = {
       color2:"#252525"
     },
     {
-      h3: "Started to learn HTML and CSS",
+      h3: "Start to learn HTML and CSS",
       h4: "",
       p: "",
       time: "August 2018",
@@ -34,15 +34,15 @@ export const dataE = {
       color2:"#252525"
     },
     {
-      h3: "Approached React JS",
+      h3: "Approaching React JS",
       h4: "",
-      p: "By my self",
+      p: "By myself",
       time: "April 2019",
       color1:"#314e52",
       color2:"#252525"
     },
     {
-      h3: "My first project was written using React and Node JS",
+      h3: "My first project was written by React and Node JS",
       h4: "",
       p: "",
       time: "June 2020",
@@ -51,7 +51,7 @@ export const dataE = {
       link:"https://oyasumy.github.io/react-demo-bookstore/"
     },
     {
-      h3: "Learn more about React Native",
+      h3: "Learning more about React Native",
       h4: "",
       p: "",
       time: "September 2020",
@@ -91,30 +91,42 @@ export const dataV = {
       h4: "Chuyên ngành chính Công Nghệ Thông Tin",
       p: "Sinh Viên",
       time: "Tháng 9 - 2017",
+      color1:"#ec4646",
+      color2:"#252525"
     },
     {
       h3: "Bắt đầu học về HTML và CSS",
       h4: "",
       p: "",
       time: "Tháng 8 - 2018",
+      color1:"#1687a7",
+      color2:"#252525"
     },
     {
       h3: "Tiếp cận React JS",
       h4: "",
       p: "Tự Học",
       time: "Tháng 4 - 2019",
+      color1:"#314e52",
+      color2:"#252525"
     },
     {
       h3: "Dự án đầu tiên sử dụng React và Node JS",
       h4: "",
       p: "",
       time: "Tháng 6 - 2020",
+      color1:"#11698e",
+      color2:"#252525",
+      link:"https://oyasumy.github.io/react-demo-bookstore/"
     },
     {
       h3: "Học thêm về React Native",
       h4: "",
       p: "",
       time: "Tháng 9 2020",
+      color1:"#f4c983",
+      color2:"#28527a",
+      link:"https://oyasumy.github.io/react-demo-bookstore/"
     },
   ],
   interest: {
